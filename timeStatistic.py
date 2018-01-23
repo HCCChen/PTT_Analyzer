4d6e4557cb5fe7967527b6658f0b4ed0bd9c9aca
HCCChen <ccx97u@gmail.com>
2018-01-23 16:50:42 +0800

    [Develope] Save statistic result by json; add some item in result
    Change list:
    - Save statistic result by json.
    - Add some item in statistic result to lower the UI loading

--- timeStatistic.py
+++ timeStatistic.py
@@ -27,32 +27,46 @@ class TimeStatistic:
     # Return: article counter for each author
     def getAuthorStatistic(self, year, month, day):
         authorCount = {}
+        authorNumber = 0
         for idx in range(0, len(self.articleIndex)):
             if self.articleIndex[idx]['author'] in authorCount:
                 authorCount[self.articleIndex[idx]['author']] += 1
             else:
                 authorCount[self.articleIndex[idx]['author']] = 1
+                authorNumber += 1
 
-        return authorCount
+        return json.dumps({'authorCount':authorCount, 'authorNumber':authorNumber})
 
     # Get time distribution for all article
     # Return: article count for each time slot (a hour)
     def getArticleTimeDistribution(self):
         articleTimeCount = [0] * 24
+        firstTimeStamp = self.articleIndex[0]['timeStamp']
+        lastTimeStamp = self.articleIndex[0]['timeStamp']
+
         for idx in range(0, len(self.articleIndex)):
+            # Check first/last timestamp in the list
+            if firstTimeStamp > self.articleIndex[idx]['timeStamp']:
+                firstTimeStamp = self.articleIndex[idx]['timeStamp']
+            if lastTimeStamp < self.articleIndex[idx]['timeStamp']:
+                lastTimeStamp = self.articleIndex[idx]['timeStamp']
+
             # Add timestmap for GMT+8
             dayTimeStamp = (self.articleIndex[idx]['timeStamp'] + 28800) % 86400
             timeSlot = (int) (dayTimeStamp / 3600)
             articleTimeCount[timeSlot] += 1
-        return articleTimeCount
+
+        return json.dumps({'firstTimeStamp':firstTimeStamp, 'lastTimeStamp':lastTimeStamp, 'maxCount':max(articleTimeCount), 'minCount':min(articleTimeCount), 'articleTimeCount':articleTimeCount})
 
     # Get time distrubution for each comment
-    # Return: comment count for each time slot (a half hour)
+    # Return: comment count for each time slot (a half hour), json format
     def getCommentTimeDistrbution(self):
         # one slot is a half hour, record to 7 days, last one is more than 7 days
         commentTimeCount = [0] * 337
         # Error count - Record negative value
-        errorNegativeTimeStamp = 0
+        countForNegativeTimeStamp = 0
+        countForOverFlowTimeStamp = 0
+
         for listIdx in range(0, len(self.articleIndex)):
             articleMeta = self.loadArticleMeta(self.articleIndex[listIdx]['filePath'])
             postTimeStamp = articleMeta['timeStamp']
@@ -61,12 +75,14 @@ class TimeStatistic:
                 timeSlot = (int)(diffTime/1800)
                 if timeSlot > 336:
                     timeSlot = 336
+                    countForOverFlowTimeStamp += 1
                 elif timeSlot < 0:
                     timeSlot = 336
-                    errorNegativeTimeStamp += 1
-                commentTimeCount[timeSlot] += 1
-        print('getCommentTimeDistrbution done, error time stamp is ', errorNegativeTimeStamp)
-        return commentTimeCount
+                    countForNegativeTimeStamp += 1
+                else:
+                    commentTimeCount[timeSlot] += 1
+
+        return json.dumps({'maxCount':max(commentTimeCount), 'minCount':min(commentTimeCount), 'negativeTimeStamp':countForNegativeTimeStamp, 'overflowTimeStamp':countForOverFlowTimeStamp, 'timeDistribution':commentTimeCount})
 
 # Main function
 if __name__ == '__main__':
@@ -76,12 +92,12 @@ if __name__ == '__main__':
 
     ## Example for getAuthorStatistic ##
     #authorCount_20180118 = timeStatistic.getAuthorStatistic(2018,1,18)
-    #authorCount_20180118_sorted = sorted(authorCount_20180118.items(), key=lambda x: x[1])
-    #print(authorCount_20180118_sorted)
+    #print(authorCount_20180118)
 
     ## Example for getArticleTimeDistribution ##
     #articleTimeDistribution = timeStatistic.getArticleTimeDistribution()
+    #print(articleTimeDistribution)
     #fp = open('ArticleTimeDistribution.json', 'w')
     #fp.write(json.dumps(articleTimeDistribution, ensure_ascii=False))
 
-    print(timeStatistic.getCommentTimeDistrbution())
+    #print(timeStatistic.getCommentTimeDistrbution())
